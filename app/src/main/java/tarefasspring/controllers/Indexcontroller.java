7aa442eebfcb3a9d9b147d93fc924948cbbc4c66
MaikProgramador <maikbigdata@gmail.com>
2023-04-25 19:22:53 +0000

    Spring Right

---
+++ app/src/main/java/tarefasspring/controllers/Indexcontroller.java
@@ -0,0 +1,13 @@
+package tarefasspring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Indexcontroller {
+    @RequestMapping("/")
+    public String home(){
+        return "home.jsp";
+    }
+    
+}
